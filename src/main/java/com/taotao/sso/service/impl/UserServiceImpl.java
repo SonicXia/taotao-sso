ecfae83eedb3c3178060891d05e2b34292d25731
夏晓峰 <sonicdxf@sina.com>
2016-06-29 10:42:40 +0800

    no message

--- src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
+++ src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.CookieUtils;
 import com.taotao.common.utils.JsonUtils;
 import com.taotao.mapper.TbUserMapper;
 import com.taotao.pojo.TbUser;
@@ -69,7 +73,7 @@ public class UserServiceImpl implements UserService {
 		user.setUpdated(new Date());
 		user.setCreated(new Date());
 		//md5加密
-		user.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+		user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
 		userMapper.insert(user);
 		return TaotaoResult.ok();
 	}
@@ -78,7 +82,8 @@ public class UserServiceImpl implements UserService {
 	 * 用户登录
 	 */
 	@Override
-	public TaotaoResult userLogin(String username, String password) {
+	public TaotaoResult userLogin(String username, String password, 
+			HttpServletRequest request, HttpServletResponse response) {
 		
 		//创建查询条件
 		TbUserExample example = new TbUserExample();
@@ -101,6 +106,9 @@ public class UserServiceImpl implements UserService {
 		jedisClient.set(REDIS_USER_SESSION_KEY + ":" + token, JsonUtils.objectToJson(user));
 		//设置session的过期时间
 		jedisClient.expire(REDIS_USER_SESSION_KEY + ":" + token, SSO_SESSION_EXPIRE);
+		
+		//添加写cookie的逻辑，cookie的有效期是关闭浏览器就失效
+		CookieUtils.setCookie(request, response, "TT_TOKEN", token);
 		
 		return TaotaoResult.ok(token);
 	}
